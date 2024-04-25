98106fe62e358bea99bbc31a3f47be68a818523d
Roberto <roberto@rezende.biz>
2024-04-25 20:51:09 -0300

    Movido para o diretório acima

--- build.sh
+++ build.sh
@@ -10,11 +10,11 @@
 
 set -e
 
-docker build --file preparar_ambiente.Dockerfile --tag rfrezende/app-projeto-ada:preparar-ambiente .
+docker build --file ./docker_files/preparar_ambiente.Dockerfile --tag rfrezende/app-projeto-ada:preparar-ambiente .
 docker push rfrezende/app-projeto-ada:preparar-ambiente
 
-docker build --file producer.Dockerfile --tag rfrezende/app-projeto-ada:producer .
+docker build --file ./docker_files/producer.Dockerfile --tag rfrezende/app-projeto-ada:producer .
 docker push rfrezende/app-projeto-ada:producer
 
-docker build --file consumer.Dockerfile --tag rfrezende/app-projeto-ada:consumer .
+docker build --file ./docker_files/consumer.Dockerfile --tag rfrezende/app-projeto-ada:consumer .
 docker push rfrezende/app-projeto-ada:consumer

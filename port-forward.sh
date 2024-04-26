80fe0fd191b7b90be463e4cb0d5c61f08dc66ec4
Roberto <roberto@rezende.biz>
2024-04-26 10:26:07 -0300

    kill port forward

--- port-forward.sh
+++ port-forward.sh
@@ -10,8 +10,13 @@
 
 set -e
 
-echo -e '\n==== Criando os redirecionamentos de porta para o ambiente do projeto ====\n\n'
-minikube kubectl -- port-forward service/rabbitmq-service 15672:15672 &
-minikube kubectl -- port-forward service/minio-service 9000:9000 &
-minikube kubectl -- port-forward service/minio-service 9001:9001 &
-minikube kubectl -- port-forward service/redis-service 8001:8001 &
+if [[ $1 == 'up' ]]; then
+    echo -e '\n==== Criando os redirecionamentos de porta para o ambiente do projeto ====\n\n'
+    minikube kubectl -- port-forward service/rabbitmq-service 15672:15672 &
+    minikube kubectl -- port-forward service/minio-service 9000:9000 &
+    minikube kubectl -- port-forward service/minio-service 9001:9001 &
+    minikube kubectl -- port-forward service/redis-service 8001:8001 &
+elif [[ $1 == 'down' ]]; then
+    echo -e '\n==== Removendo os redirecionamentos de porta para o ambiente do projeto ====\n\n'
+    ps aux | grep "kubectl port" | grep -v grep | awk '{print $2}' | xargs -I {} kill {}
+fi

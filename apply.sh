4dfcbf05eb76344420f9152589e428a61cb1aa4d
Roberto <roberto@rezende.biz>
2024-04-26 10:04:01 -0300

    rede do MinIO

--- apply.sh
+++ apply.sh
@@ -19,17 +19,13 @@ for deployment in ./deployment/*.yaml; do
     minikube kubectl -- apply -f $deployment
 done
 
-echo -e '\n\n==== Esperando 20 segundos para os serviços subirem ====\n'
+echo -e '\n==== Criando os redirecionamentos de porta para o ambiente do projeto ====\n\n'
+minikube kubectl -- port-forward service/rabbitmq-service 15672:15672 &
+minikube kubectl -- port-forward service/minio-service 9000:9000 &
+minikube kubectl -- port-forward service/minio-service 9001:9001 &
+minikube kubectl -- port-forward service/redis-service 8001:8001 &
 
-# Só porque fica bonitinho
-BAR='####################'
-echo -e '\t┌────────────────────┐'
-for i in {1..20}; do
-    echo -ne "\r\t ${BAR:0:$i}"
-    sleep 1
-done
-
-echo -e '\n\n==== Exibindo o log do consumer (pressione "Control + c" para sair) ===='
-echo -e '\n==== Pode demorar para começar a aparecer os relatórios ====\n\n'
+echo -e '\n\n==== Comando para exibir o log do consumer (pressione "Control + c" para sair) ===='
+echo -e '\n==== Pode demorar alguns minutos para subir tudo e começar a gerar os relatórios  ====\n\n'
 consumer=$(minikube kubectl -- get pods | grep consumer | cut -d' ' -f 1)
-minikube kubectl -- logs --follow $consumer
+echo minikube kubectl -- logs --follow $consumer --namespace=projeto-orquestracao
